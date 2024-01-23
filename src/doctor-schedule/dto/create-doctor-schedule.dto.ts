0e63365339d1791f57b9d0684197d0a22cad9e22
Taras Kozodoi <acateely@gmail.com>
2024-01-23 15:22:22 +0200

    feat: add dto for doctor schedule

--- src/doctor-schedule/dto/create-doctor-schedule.dto.ts
+++ src/doctor-schedule/dto/create-doctor-schedule.dto.ts
@@ -1,1 +1,9 @@
-export class CreateDoctorScheduleDto {}
+import { IsISO8601 } from 'class-validator'
+
+export class CreateDoctorScheduleDto {
+  @IsISO8601()
+  public readonly startTime: string
+
+  @IsISO8601()
+  public readonly endTime: string
+}
