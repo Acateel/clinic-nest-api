69545f625f4aaeb7762f6e386ab90f67e3ab7042
Taras Kozodoi <acateely@gmail.com>
2024-01-23 14:25:56 +0200

    feat: add CRUD func for doctors

--- src/doctor/doctor.service.ts
+++ src/doctor/doctor.service.ts
@@ -1,26 +1,66 @@
-import { Injectable } from '@nestjs/common';
-import { CreateDoctorDto } from './dto/create-doctor.dto';
-import { UpdateDoctorDto } from './dto/update-doctor.dto';
+import { Injectable } from '@nestjs/common'
+import { CreateDoctorDto } from './dto/create-doctor.dto'
+import { UpdateDoctorDto } from './dto/update-doctor.dto'
+import { Repository } from 'typeorm'
+import { Doctor } from './entities/doctor.entity'
+import { InjectRepository } from '@nestjs/typeorm'
 
 @Injectable()
 export class DoctorService {
-  create(createDoctorDto: CreateDoctorDto) {
-    return 'This action adds a new doctor';
+  constructor(
+    @InjectRepository(Doctor)
+    private doctorRepo: Repository<Doctor>
+  ) {}
+
+  async create({ firstName, lastName, specialty }: CreateDoctorDto) {
+    const doctor = new Doctor()
+
+    doctor.firstName = firstName
+    doctor.lastName = lastName
+    doctor.specialty = specialty
+
+    const result = await this.doctorRepo.save(doctor)
+
+    return result
   }
 
-  findAll() {
-    return `This action returns all doctor`;
+  async findAll() {
+    let doctorsQuery = this.doctorRepo
+      .createQueryBuilder('doctor')
+      .addSelect('count(appointment_entity.id)', 'app_count')
+      .leftJoin('doctor.appointments', 'appointment_entity')
+      .groupBy('doctor.id')
+      .addOrderBy('app_count', 'DESC')
+
+    const doctors: any[] = await doctorsQuery.getMany()
+
+    return doctors
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} doctor`;
+  async findOne(id: number) {
+    const doctor = await this.doctorRepo.findOneBy({ id })
+
+    return doctor
   }
 
-  update(id: number, updateDoctorDto: UpdateDoctorDto) {
-    return `This action updates a #${id} doctor`;
+  async update(
+    id: number,
+    { firstName, lastName, specialty }: UpdateDoctorDto
+  ) {
+    const doctor = await this.doctorRepo.findOneBy({ id })
+
+    doctor.firstName = firstName
+    doctor.lastName = lastName
+    doctor.specialty = specialty
+
+    const result = await this.doctorRepo.save(doctor)
+
+    return result
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} doctor`;
+  async remove(id: number) {
+    const result = await this.doctorRepo.delete(id)
+
+    return result
   }
 }
