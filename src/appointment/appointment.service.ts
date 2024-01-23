20f61e22134df74db7d04476d84dd39ccfbf74fe
Taras Kozodoi <acateely@gmail.com>
2024-01-23 15:23:27 +0200

    feat: add CRUD func for doctor-schedules

--- src/appointment/appointment.service.ts
+++ src/appointment/appointment.service.ts
@@ -1,26 +1,48 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAppointmentDto } from './dto/create-appointment.dto';
-import { UpdateAppointmentDto } from './dto/update-appointment.dto';
+import { Injectable } from '@nestjs/common'
+import { CreateAppointmentDto } from './dto/create-appointment.dto'
+import { UpdateAppointmentDto } from './dto/update-appointment.dto'
+import { LessThanOrEqual, MoreThanOrEqual, Repository } from 'typeorm'
+import { Appointment } from './entities/appointment.entity'
+import { InjectRepository } from '@nestjs/typeorm'
 
 @Injectable()
 export class AppointmentService {
+  constructor(
+    @InjectRepository(Appointment)
+    private appointmentRepo: Repository<Appointment>
+  ) {}
+
   create(createAppointmentDto: CreateAppointmentDto) {
-    return 'This action adds a new appointment';
+    return 'This action adds a new appointment'
   }
 
   findAll() {
-    return `This action returns all appointment`;
+    return `This action returns all appointment`
+  }
+
+  async findByDoctorSchedule(doctorId: number, startTime: Date, endTime: Date) {
+    const appointmentsBySchedule = await this.appointmentRepo.find({
+      where: {
+        doctor: {
+          id: doctorId,
+        },
+        startTime: MoreThanOrEqual(startTime),
+        endTime: LessThanOrEqual(endTime),
+      },
+    })
+
+    return appointmentsBySchedule
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} appointment`;
+    return `This action returns a #${id} appointment`
   }
 
   update(id: number, updateAppointmentDto: UpdateAppointmentDto) {
-    return `This action updates a #${id} appointment`;
+    return `This action updates a #${id} appointment`
   }
 
   remove(id: number) {
-    return `This action removes a #${id} appointment`;
+    return `This action removes a #${id} appointment`
   }
 }
--- src/doctor-schedule/doctor-schedule.module.ts
+++ src/doctor-schedule/doctor-schedule.module.ts
@@ -3,10 +3,17 @@ import { DoctorScheduleService } from './doctor-schedule.service'
 import { DoctorScheduleController } from './doctor-schedule.controller'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { DoctorSchedule } from './entities/doctor-schedule.entity'
+import { DoctorModule } from 'src/doctor/doctor.module'
+import { AppointmentModule } from 'src/appointment/appointment.module'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([DoctorSchedule])],
+  imports: [
+    TypeOrmModule.forFeature([DoctorSchedule]),
+    DoctorModule,
+    AppointmentModule,
+  ],
   controllers: [DoctorScheduleController],
   providers: [DoctorScheduleService],
+  exports: [DoctorScheduleService],
 })
 export class DoctorScheduleModule {}
