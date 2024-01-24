7edb49c50447f4a3d0ce568a88c8f26224612bbf
Taras Kozodoi <acateely@gmail.com>
2024-01-24 23:05:21 +0200

    feat: add user functions

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,26 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsEnum,
+  IsMobilePhone,
+  IsOptional,
+  IsString,
+  Length,
+} from 'class-validator'
+import { UserRole } from '../entities/user.entity'
+
+export class CreateUserDto {
+  @IsOptional()
+  @IsEmail()
+  public readonly email: string
+
+  @IsOptional()
+  @Length(4, 15)
+  @IsMobilePhone()
+  public readonly phoneNumber: string
+
+  @IsString()
+  public readonly password: string
+
+  @IsEnum(UserRole)
+  public readonly role: UserRole
+}
