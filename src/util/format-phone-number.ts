7ff05c53c03303e3b152ad1edacff4df13271ef3
Taras Kozodoi <acateely@gmail.com>
2024-01-27 10:56:21 +0200

    fix: fix format phone number

--- src/util/format-phone-number.ts
+++ src/util/format-phone-number.ts
@@ -2,10 +2,11 @@
  * Convert phone number in format: +XXXXXXXX
  */
 export const formatPhoneNumber = (phoneNumber: string) => {
+  if (phoneNumber) {
     var cleaned = phoneNumber.replace(/\D/g, '')
     if (cleaned.length >= 8) {
       return `+${cleaned}`
     }
-    return null
   }
-  
+  return null
+}
