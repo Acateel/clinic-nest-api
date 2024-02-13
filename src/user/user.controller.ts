c662e126d8977dbe206c862157395c16b8c65d95
Taras Kozodoi <acateely@gmail.com>
2024-02-13 20:08:03 +0200

    feat: use roles guard in users endpoints

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,12 @@
-import { Controller, Get, Param, Delete } from '@nestjs/common'
+import { Controller, Get, Param, Delete, UseGuards } from '@nestjs/common'
 import { UserService } from './user.service'
+import { Roles } from 'src/auth/guard/roles.decorator'
+import { UserRole } from 'src/database/entities/user.entity'
+import { RolesGuard } from 'src/auth/guard/roles.guard'
 
 @Controller('users')
+@UseGuards(RolesGuard)
+@Roles(UserRole.Admin)
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
