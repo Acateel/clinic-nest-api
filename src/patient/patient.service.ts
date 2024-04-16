cdafef63bee3e761daec7083c20007d7b1314881
Taras Kozodoi <acateely@gmail.com>
2024-04-16 22:16:32 +0300

    fix: move src/util into common folder

--- src/patient/patient.service.ts
+++ src/patient/patient.service.ts
@@ -4,7 +4,7 @@ import { UpdatePatientDto } from './dto/update-patient.dto'
 import { Repository } from 'typeorm'
 import { Patient } from '../database/entities/patient.entity'
 import { InjectRepository } from '@nestjs/typeorm'
-import { formatPhoneNumber } from 'src/util'
+import { formatPhoneNumber } from 'src/common/format-phone-number'
 
 @Injectable()
 export class PatientService {
