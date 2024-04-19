2ddb36a9294bab73617c3116d9384d896fd26d39
Taras Kozodoi <acateely@gmail.com>
2024-04-19 17:10:43 +0300

    fix: move guards to endpoints

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -5,21 +5,25 @@ import { UserRole } from 'src/common/user-role-enum'
 import { RolesGuard } from 'src/auth/guard/roles.guard'
 
 @Controller('users')
-@UseGuards(RolesGuard)
-@Roles(UserRole.Admin)
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @UseGuards(RolesGuard)
+  @Roles(UserRole.Admin)
   @Get()
   findAll() {
     return this.userService.findAll()
   }
 
+  @UseGuards(RolesGuard)
+  @Roles(UserRole.Admin)
   @Get(':id')
   findOne(@Param('id') id: number) {
     return this.userService.findOne(id)
   }
 
+  @UseGuards(RolesGuard)
+  @Roles(UserRole.Admin)
   @Delete(':id')
   remove(@Param('id') id: number) {
     return this.userService.remove(id)
