438154442ac41608fc5392dfe5026bc5752bc7e8
Taras Kozodoi <acateely@gmail.com>
2024-05-17 17:57:59 +0300

    feat: add separate by weeks

--- src/analytic/analytic.service.ts
+++ src/analytic/analytic.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@nestjs/common'
-import { InjectDataSource, InjectEntityManager } from '@nestjs/typeorm'
-import { DataSource, EntityManager } from 'typeorm'
+import { InjectEntityManager } from '@nestjs/typeorm'
+import { EntityManager } from 'typeorm'
 import { DoctorAppointmentsSummary } from 'src/database/entities/doctor-appointments-summary.entity'
+import { Departament } from 'src/database/entities/departament.entity'
+import { Week, getWeeksArray } from './util'
 
 @Injectable()
 export class AnalyticService {
   constructor(
     @InjectEntityManager()
-    private entityManager: EntityManager,
-    @InjectDataSource()
-    private dataSourse: DataSource
+    private entityManager: EntityManager
   ) {}
 
   async comptuteAppointmentsAnalytics(
@@ -20,6 +20,108 @@ export class AnalyticService {
   ) {
     const summary = await this.entityManager.find(DoctorAppointmentsSummary)
 
-    return summary
+    const departaments = await this.entityManager
+      .getTreeRepository(Departament)
+      .findTrees()
+
+    const allWeeks = getWeeksArray(
+      summary[0].weekMinDate,
+      summary[summary.length - 1].weekMinDate
+    )
+
+    const currentPeriod = allWeeks.map((week) => {
+      const key = `${week.year}-${week.month}:${week.weekNumberInMonth}`
+      const value = this.wrapDepartements(
+        departaments,
+        summary.filter((element) => element.weekNumber == week.weekNumber)
+      )
+
+      return { [key]: value }
+    })
+
+    return {
+      topDoctor: {},
+      currentPeriod: Object.assign({}, ...currentPeriod),
+      previosPeriod: {},
+    }
+  }
+
+  wrapDepartements(
+    departaments: Departament[],
+    summary: DoctorAppointmentsSummary[]
+  ) {
+    const nodes: Departament[] = this.getNodesBFS(departaments)
+    let wrappedNodes = {}
+
+    nodes.reverse().forEach((node) => {
+      if (node.children.length !== 0) {
+        const wrapedChilderNodes = this.getWrapedChildrenNodes(
+          node.children,
+          wrappedNodes
+        )
+
+        if (
+          Object.keys(wrapedChilderNodes).length !== 0 &&
+          this.isWrappedChildrenNotEmpty(wrapedChilderNodes)
+        ) {
+          wrappedNodes = {
+            ...wrappedNodes,
+            [node.name]: wrapedChilderNodes,
+          }
+          return
+        }
+      }
+
+      wrappedNodes = {
+        ...wrappedNodes,
+        [node.name]: this.wrapDoctorsSummary(node, summary),
+      }
+    })
+
+    return this.getWrapedChildrenNodes(departaments, wrappedNodes)
+  }
+
+  getNodesBFS(departaments: Departament[]) {
+    let nodes: Departament[] = []
+
+    departaments.forEach((departament) => nodes.push(departament))
+
+    let index = 0
+    while (index < nodes.length) {
+      nodes[index]?.children?.forEach((child) => nodes.push(child))
+      index++
+    }
+
+    return nodes
+  }
+
+  wrapDoctorsSummary(
+    departament: Departament,
+    summary: DoctorAppointmentsSummary[]
+  ) {
+    const summaryByDepartament = summary.filter((element) =>
+      element.departamentIds.includes(departament.id)
+    )
+
+    return summaryByDepartament ?? {}
+  }
+
+  getWrapedChildrenNodes(children: Departament[], wrappedNodes: any) {
+    let childrenWrappedNodes = {}
+
+    children.forEach((child) => {
+      childrenWrappedNodes = {
+        ...childrenWrappedNodes,
+        [child.name]: wrappedNodes[child.name],
+      }
+    })
+
+    return childrenWrappedNodes
+  }
+
+  isWrappedChildrenNotEmpty(wrapedChildren: any) {
+    return Object.values(wrapedChildren).every(
+      (value) => Object.keys(value).length !== 0
+    )
   }
 }
---
+++ src/analytic/util.ts
@@ -0,0 +1,73 @@
+export interface Week {
+  year: number
+  month: number
+  weekNumberInMonth: number
+  weekNumber: number
+}
+
+export function getWeeksArray(startTime: Date, endTime: Date): Week[] {
+  let weeks: Week[] = []
+
+  let initYear = startTime.getFullYear()
+  let initWeekNumber = getWeekYear(startTime)
+
+  const endYear = endTime.getFullYear()
+  const endWeekNumber = getWeekYear(endTime)
+
+  while (initYear < endYear) {
+    const weeksInYear = getWeekYear(new Date(initYear, 11, 31))
+    while (initWeekNumber <= weeksInYear) {
+      weeks.push({
+        year: initYear,
+        month: getMonthByWeekNumber(initWeekNumber) + 1,
+        weekNumberInMonth: getWeekMonth(initYear, initWeekNumber),
+        weekNumber: initWeekNumber,
+      })
+      initWeekNumber++
+    }
+    initYear++
+    initWeekNumber = 1
+  }
+
+  while (initWeekNumber <= endWeekNumber) {
+    weeks.push({
+      year: initYear,
+      month: getMonthByWeekNumber(initWeekNumber) + 1,
+      weekNumberInMonth: getWeekMonth(initYear, initWeekNumber),
+      weekNumber: initWeekNumber,
+    })
+    initWeekNumber++
+  }
+
+  return weeks
+}
+
+function getWeekYear(d: Date) {
+  var date = new Date(d.getTime())
+  date.setHours(0, 0, 0, 0)
+  date.setDate(date.getDate() + 3 - ((date.getDay() + 6) % 7))
+  var week1 = new Date(date.getFullYear(), 0, 4)
+  return (
+    1 +
+    Math.round(
+      ((date.getTime() - week1.getTime()) / 86400000 -
+        3 +
+        ((week1.getDay() + 6) % 7)) /
+        7
+    )
+  )
+}
+
+function getMonthByWeekNumber(weekNumber: number) {
+  return new Date(1000 * 60 * 60 * 24 * 7 * weekNumber).getMonth()
+}
+
+function getWeekMonth(year: number, weekNumber: number) {
+  const month = getMonthByWeekNumber(weekNumber)
+
+  const date = new Date(year, month, 1)
+
+  const firstWeekInMonth = getWeekYear(date)
+
+  return weekNumber - firstWeekInMonth + 1
+}
